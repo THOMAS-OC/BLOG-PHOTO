ddad7c9881f0164568161ca01feccc293e1164e1
= <=>
2022-07-02 15:49:57 +0200

    Création d'une app de blog photo avec express

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv')
+const db = require('./model/index')
+dotenv.config()
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+// ROUTE PAGE D'ACCUEIL
+app.get("/", (req, res) => {
+    res.redirect("/home")
+})
+
+// ROUTE PAGE D'ACCUEIL
+app.get("/home", (req, res) => {
+    db.photoUpload.find({}, (err, docs)=>{
+        images = docs
+        console.log(images);
+    })
+    res.render("home.ejs", {images})
+})
+
+// ROUTE PAGE CONTACT
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs")
+})
+
+// ROUTE POST UPLOAD EMAIL
+app.post("/contact/sendEmail", (req, res) => {
+    res.render("post.ejs")
+})
+
+// Route formulaire d'upload
+app.get("/upload", (req, res) => {
+    res.render("upload.ejs")
+})
+
+// Route POST upload photo
+app.post("/upload/send", (req, res) => {
+
+    let authorize = false
+
+    if (req.body.secretCode == process.env.CODE){
+        authorize = true
+        console.log('Enregistrement en BDD');
+        db.photoUpload.create({
+            urlFile: req.body.urlFile,
+            urlFileCompressed: req.body.urlFileCompressed,
+            alt: req.body.alt,
+            categorie: req.body['categorie-select']
+        })
+    }
+    else {
+        console.log('Intrusion');
+
+    }
+    res.redirect("/")
+})
+
+app.listen(3000, () => {
+    console.log("Serveur lancé");
+})
---
+++ model/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema
+
+mongoose.connect('mongodb://localhost:27017/SITE_PHOTO')
+
+const uploadSchema = schema({
+    urlFile: String,
+    urlFileCompressed: String,
+    alt: String,
+    categorie: String,
+})
+
+module.exports.photoUpload = mongoose.model('photo', uploadSchema)
