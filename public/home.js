3e64ff8658ed6a31479da6c4f40d79a5fb29e636
= <=>
2022-07-03 16:58:23 +0200

    Nouvelle animation de texte

--- public/home.js
+++ public/home.js
@@ -1,14 +1,14 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml12');
+var textWrapper = document.querySelector('.ml13');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: false})
   .add({
-    targets: '.ml12 .letter',
-    translateX: [40,0],
+    targets: '.ml13 .letter',
+    translateY: [100,0],
     translateZ: 0,
     opacity: [0,1],
     easing: "easeOutExpo",
-    duration: 1500,
-    delay: (el, i) => 500 + 30 * i
-  });
+    duration: 1400,
+    delay: (el, i) => 1000 + 70 * i
+  })
